9d2e3f62910391a72c952a807eb60835b8d27999
Beat Schärz <beatschaerz@hotmail.com>
2018-10-01 19:36:00 +0200

    highlight current

--- Assignment 2/menu.php
+++ Assignment 2/menu.php
@@ -11,6 +11,12 @@
 		);
 	echo "<ul class='menu'>";
 	foreach ($menu_array as $menu){
-		echo "<li><a href='".$menu["link"]."' id='menu'>".$menu["name"]."</a></li>";
+		$current = explode("/", $_SERVER["PHP_SELF"]);
+		$script_name = array_values(array_slice($current, -1))[0];
+		echo "<li><a href='".$menu["link"]."' class='menuentry' ";
+		if ($menu["link"] == $script_name){
+			echo "id='menuselected'";
+		}
+		echo ">".$menu["name"]."</a></li>";
 	}
 	echo "</ul>";
