fa2ded45ea12fad5fcfdf944da79229051eda2a9
Dario Furigo <dario.furigo@itpoint.ch>
2018-11-26 19:18:23 +0100

    New getInstance()

--- Assignment 2/objects/DB.php
+++ Assignment 2/objects/DB.php
@@ -10,13 +10,14 @@ class DB extends mysqli{
 	static public function getInstance() {
 		if ( !self::$instance) {
 			@self::$instance = new DB();
+			if (!self::$instance->set_charset("utf8")){
+				die ("Error loading character set utf8 ".$instance->error);
+			}
+			if (self::$instance->connect_errno > 0){
+				die ("Unable to connect to database: ".$instance->connect_errno);
+			}
 		}
-		if (self::$instance->connect_errno > 0){
-			die ("Unable to connect to database: ".$instance->connect_errno);
-		}
-		if (!self::$instance->set_charset("utf8")){
-			die ("Error loading character set utf8 ".$instance->error);
-		}
+		
 		return self::$instance;
 	}
 
