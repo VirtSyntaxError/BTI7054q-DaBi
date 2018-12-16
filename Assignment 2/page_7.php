30cbe8fe79926e34206b49e76f2cb91c9e6da4f1
Beat Schärz <beatschaerz@hotmail.com>
2018-12-16 10:47:54 +0100

    empty cart now possible without js

--- Assignment 2/page_7.php
+++ Assignment 2/page_7.php
@@ -6,7 +6,10 @@ echo '<article id="cart">';
 $cart = $_SESSION['cart'];
 $cart->render(false);
 if (!$cart->isEmpty()) {
-	echo '<input type="button" value="'.t("EMPTYCART").'" onclick="emptyCart()">';
+	//echo '<input type="button" value="'.t("EMPTYCART").'" onclick="emptyCart()">';
+	echo '<form method="post" action="ajax/emptycart.php?red=7">';
+	echo '<input type="Submit" value="'.t("EMPTYCART").'" onclick="emptyCart(); return false">';
+	echo '</form>';
 	echo '<form method="post" action="index.php?id=4">';
 	echo '<input type="submit" value="'.t("CHECKOUT").'">';
 	echo '</form>';
