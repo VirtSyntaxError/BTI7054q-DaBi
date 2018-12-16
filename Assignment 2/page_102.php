726f150cfec4589d87fbd05a655a04478ac85b2e
VirtSyntaxError <dario.furigo@itpoint.ch>
2018-12-16 19:39:41 +0100

    Remember language after logout

--- Assignment 2/page_102.php
+++ Assignment 2/page_102.php
@@ -3,7 +3,9 @@ if(!isset($_SESSION)){
 	session_start(); 
 }
 $cart = $_SESSION['cart'];
+$lang = $_SESSION['lang'];
 $_SESSION = [];
 setcookie(session_name(),'',1);
 $_SESSION['cart'] = $cart;
+$_SESSION['lang'] = $lang;
 header("Location: index.php?id=100");
